a013e4080b816fb9727fc8aac0c39060d38f83f6
RobertoLopez00 <robertolp349@gmail.com>
2024-07-26 11:22:29 -0600

    Subida del proyecto

---
+++ src/app/data.js
@@ -0,0 +1,40 @@
+export const data = [
+  {
+    id: 1,
+    title: "Cien años de soledad",
+    price: 100,
+    urlImage: "https://images.penguinrandomhouse.com/cover/9780525562443",
+    quantity: 1,
+  },
+  {
+    id: 2,
+    title: "El señor de los anillos(Trilogía)",
+    price: 190,
+    urlImage:
+      "https://www.planetadelibros.com/usuaris/libros/fotos/358/original/portada_pack-trilogia-el-senor-de-los-anillos_j-r-r-tolkien_202206071544.jpg",
+    quantity: 1,
+  },
+  {
+    id: 3,
+    title: "Cuentos de Barro",
+    price: 30,
+    urlImage: "https://m.media-amazon.com/images/I/41QZEmcDLsL.jpg",
+    quantity: 1,
+  },
+  {
+    id: 4,
+    title: "Tierra de Infancia",
+    price: 30,
+    urlImage:
+      "https://marketsvstg.blob.core.windows.net/marketsv/0011705_tierra-de-infancia_510.jpeg",
+    quantity: 1,
+  },
+  {
+    id: 5,
+    title: "Harry Potter Pack",
+    price: 390,
+    urlImage:
+      "https://http2.mlstatic.com/D_NQ_NP_946003-MLA52123883936_102022-O.webp",
+    quantity: 1,
+  },
+];
